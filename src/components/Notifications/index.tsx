11fd6b417c56877ae42a907dbf0e142e490ff24d
Filip Witosz <filip.witosz@gmail.com>
2021-01-20 10:23:14 +0100

    Continuing on notifications.

--- src/components/Notifications/index.tsx
+++ src/components/Notifications/index.tsx
@@ -6,7 +6,21 @@ export interface IReqoreNotificationsWrapperProps {
   position: 'TOP' | 'BOTTOM';
 }
 
-const StyledNotificationsWrapper = styled.div``;
+export type IReqoreNotificationsPosition =
+  | 'TOP'
+  | 'BOTTOM'
+  | 'LEFT'
+  | 'RIGHT'
+  | 'TOP-LEFT'
+  | 'TOP-RIGHT'
+  | 'BOTTOM-LEFT'
+  | 'BOTTOM-RIGHT';
+
+const StyledNotificationsWrapper = styled.div<{
+  position: IReqoreNotificationsPosition;
+}>`
+  position: fixed;
+`;
 
 const ReqoreNotificationsWrapper: React.FC<IReqoreNotificationsWrapperProps> = ({
   children,
