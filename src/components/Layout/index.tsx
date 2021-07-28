791d6e0a8d443afb62f87ea3c214da5f386e5e4d
Foxhoundn <filip.witosz+textace@gmail.com>
2021-07-28 07:20:24 +0000

    Do not force a default font

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import styled, { css } from "styled-components";
-import { IReqoreTheme } from "../../constants/theme";
-import ReqoreThemeProvider from "../../containers/ThemeProvider";
-import { changeLightness, getReadableColor } from "../../helpers/colors";
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { IReqoreTheme } from '../../constants/theme';
+import ReqoreThemeProvider from '../../containers/ThemeProvider';
+import { changeLightness, getReadableColor } from '../../helpers/colors';
 
 export interface IReqoreLayoutWrapperProps
   extends React.HTMLAttributes<HTMLDivElement> {
@@ -21,9 +21,6 @@ const StyledReqoreLayoutWrapper = styled.div<{
   font-size: 14px;
 
   * {
-    font-family: "Nunito Sans", -apple-system, ".SFNSText-Regular",
-      "San Francisco", BlinkMacSystemFont, "Segoe UI", "Helvetica Neue",
-      Helvetica, Arial, sans-serif;
     box-sizing: border-box;
   }
 
@@ -32,7 +29,7 @@ const StyledReqoreLayoutWrapper = styled.div<{
   }
 
   ${({ withSidebar, theme }) => css`
-    flex-flow: ${withSidebar ? "row" : "column"};
+    flex-flow: ${withSidebar ? 'row' : 'column'};
     background-color: ${changeLightness(theme.main, 0.02)};
     color: ${getReadableColor(theme, undefined, undefined, true)};
   `}
@@ -47,7 +44,7 @@ const ReqoreLayoutWrapper = ({
   <ReqoreThemeProvider>
     <StyledReqoreLayoutWrapper
       {...rest}
-      className={`${className || ""} reqore-layout-wrapper`}
+      className={`${className || ''} reqore-layout-wrapper`}
       withSidebar={withSidebar}
     >
       {children}
