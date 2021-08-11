770137857e75b8df9f46094e0684a3263b3af503
Foxhoundn <filip.witosz+textace@gmail.com>
2021-08-11 14:47:38 +0000

    Panel now has a callback to notify when it was collapsed / expanded

--- src/stories/Panel/Panel.stories.tsx
+++ src/stories/Panel/Panel.stories.tsx
@@ -137,6 +137,37 @@ const Template: Story<IReqoreUIProviderProps> = ({ theme, ...args }: IReqoreUIPr
             </div>
           </ReqorePanel>
           <br />
+          <h4>Callback when collapsed</h4>
+          <ReqorePanel
+            {...args}
+            rounded
+            onClose={noop}
+            collapsible
+            onCollapseChange={(isCollapsed) => {
+              alert(`Content is collapsed: ${isCollapsed.toString()}`);
+            }}
+            actions={[
+              {
+                icon: 'Settings2Line',
+                onClick: noop,
+              },
+              {
+                icon: 'UserFill',
+                onClick: noop,
+                label: 'Test',
+              },
+            ]}
+          >
+            <div style={{ padding: '15px' }}>
+              I am a message a very long message - Shadowlands has mechanisms put in place for
+              allowing players to catch up on Renown, the system of gaining favor and unlocking
+              rewards, Campaign chapters, and soulbinds within your Covenant. This system works for
+              main characters who have started late, for alts, for players who have switched
+              Covenants and are starting over, and for players who have simply missed weekly quests
+              for earning Renown due to being away from the game.
+            </div>
+          </ReqorePanel>
+          <br />
           <h4>Default collapsed</h4>
           <ReqorePanel
             {...args}
