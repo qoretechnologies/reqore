2502ff8cdc4604d5d618a3f2f8fd02fdfb51a70c
Foxhoundn <filip.witosz@gmail.com>
2021-12-02 12:15:38 +0000

    Panel header, P and Heading fixes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,6 +8,8 @@ export interface IReqoreHeaderStyle extends React.HTMLAttributes<HTMLHeadingElem
 }
 
 const style = ({ theme, intent }: IReqoreHeaderStyle) => css`
+  margin: 0;
+  padding: 0;
   color: ${intent ? theme.intents[intent] : getReadableColor(theme, undefined, undefined, true)};
 `;
 
