acd3a2bfe6f5fd4c3ab621c102baf4c275a60a0e
Foxhoundn <filip.witosz@gmail.com>
2022-09-01 07:41:19 +0000

    Sidebar story

--- src/stories/Sidebar/Sidebar.stories.tsx
+++ src/stories/Sidebar/Sidebar.stories.tsx
@@ -1,168 +1,78 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
-import { omit } from 'lodash';
-import React, { useState } from 'react';
+import { noop } from 'lodash';
 import { IQorusSidebarProps } from '../../components/Sidebar';
-import { IReqoreUIProviderProps } from '../../containers/UIProvider';
-import { ReqoreSidebar, ReqoreUIProvider } from '../../index';
+import { ReqorePanel, ReqoreSidebar } from '../../index';
 import { qorusSidebarItems } from '../../mock/menu';
+import { argManager, FlatArg, NoContentArg } from '../utils/args';
+
+const { disableArg, createArg } = argManager<IQorusSidebarProps>();
 
 export default {
-  title: 'ReQore/Sidebar',
-  component: ReqoreSidebar,
-  args: {
-    items: qorusSidebarItems,
-    path: '/',
-    isLight: false,
-    isCollapsed: false,
+  title: 'Components/Sidebar',
+  argTypes: {
+    ...disableArg('items'),
+    ...FlatArg,
+    ...createArg('path', {
+      defaultValue: '/',
+      name: 'Path',
+      description: 'Mock location path',
+      type: 'string',
+    }),
+    ...createArg('floating', {
+      control: 'boolean',
+      defaultValue: false,
+      name: 'Floating',
+      description: 'If the sidebar should be floating',
+    }),
+    ...createArg('bordered', {
+      control: 'boolean',
+      defaultValue: false,
+      name: 'Bordered',
+      description: 'If the sidebar should be bordered',
+    }),
+    ...createArg('isOpen', {
+      control: 'boolean',
+      defaultValue: false,
+      name: 'Is Open',
+      description: 'If the sidebar should be shown when floating',
+    }),
+    ...createArg('position', {
+      control: 'select',
+      options: ['left', 'right'],
+      defaultValue: 'left',
+      name: 'Sidebar position',
+      description: 'Sidebar positions - this decides which border is drawn',
+    }),
+    ...NoContentArg,
   },
 } as Meta;
 
-const Template: Story<IQorusSidebarProps & IReqoreUIProviderProps> = ({
-  theme,
-  ...args
-}: IQorusSidebarProps & IReqoreUIProviderProps) => {
-  const [isOpen, setIsOpen] = useState<boolean>(args.isOpen || true);
-
+const Template: Story<IQorusSidebarProps> = (args: IQorusSidebarProps) => {
   return (
-    <ReqoreUIProvider theme={theme} withSidebar>
-      {!args.position || args.position === 'left' ? (
-        <ReqoreSidebar {...args} onCloseClick={() => setIsOpen(false)} isOpen={isOpen} />
-      ) : null}
-      <div style={{ flex: 1, width: '100%', height: '100%' }} />
-      {args.position === 'right' ? (
-        <ReqoreSidebar {...args} onCloseClick={() => setIsOpen(false)} isOpen={isOpen} />
-      ) : null}
-    </ReqoreUIProvider>
+    <ReqoreSidebar
+      {...args}
+      items={qorusSidebarItems}
+      bookmarks={['submenu-item-2', 'another-item-1']}
+      onBookmarksChange={noop}
+      customItems={[
+        {
+          element: () => (
+            <div style={{ padding: '10px' }}>
+              <ReqorePanel intent='info' flat opacity={0.5}>
+                Hello I am a custom element!
+              </ReqorePanel>
+            </div>
+          ),
+        },
+      ]}
+    />
   );
 };
 
-const MultiTemplate: Story<IQorusSidebarProps & IReqoreUIProviderProps> = ({
-  theme,
-  ...args
-}: IQorusSidebarProps & IReqoreUIProviderProps) => (
-  <ReqoreUIProvider theme={theme} withSidebar>
-    <ReqoreSidebar {...args} />
-    <ReqoreSidebar {...omit(args, ['customTheme'])} />
-    <div style={{ flex: 1, width: '100%', height: '100%' }} />
-  </ReqoreUIProvider>
-);
-
 export const Basic = Template.bind({});
-export const Collapsed = Template.bind({});
-
-Collapsed.args = {
-  isCollapsed: true,
-  isLight: false,
-};
-
-export const WithBookmarks = Template.bind({});
-
-WithBookmarks.args = {
-  bookmarks: ['submenu-item-2', 'another-item-1'],
-  onBookmarksChange: () => true,
-};
-
-export const WithActivePath = Template.bind({});
-
-WithActivePath.args = {
-  path: '/item-3/item-2',
-};
-
-export const WithCustomElement = Template.bind({});
-
-WithCustomElement.args = {
-  customItems: [
-    {
-      element: () => <p>Hello I am a custom element!</p>,
-    },
-  ],
-};
-
-export const WithDarkTheme = Template.bind({});
-
-WithDarkTheme.args = {
-  theme: {
-    main: '#444444',
-  },
-  path: '/item-3/item-2',
-};
-
-export const WithLightTheme = Template.bind({});
-
-WithLightTheme.args = {
-  theme: {
-    main: '#ffffff',
-  },
-  path: '/item-3/item-2',
-};
-
-export const WithCustomMainColor = Template.bind({});
-
-WithCustomMainColor.args = {
-  theme: {
-    main: '#333333',
-    sidebar: {
-      main: '#692b75',
-    },
-  },
-  path: '/item-3/item-2',
-} as IReqoreUIProviderProps & IQorusSidebarProps;
-
-export const WithCustomColors = Template.bind({});
-
-WithCustomColors.args = {
-  theme: {
-    main: '#c96604',
-  },
-  path: '/item-3/item-2',
-} as IReqoreUIProviderProps & IQorusSidebarProps;
-
-export const WithCustomTheme = MultiTemplate.bind({});
-
-WithCustomTheme.args = {
-  customTheme: {
-    main: '#ffffff',
-    item: {
-      color: '#ff0000',
-    },
-  },
-  path: '/item-3/item-2',
-} as IReqoreUIProviderProps & IQorusSidebarProps;
-
-export const WithNativeTitles = Template.bind({});
-
-WithNativeTitles.args = {
-  useNativeTitle: true,
-} as IReqoreUIProviderProps & IQorusSidebarProps;
-
-export const CollapsingDisabled = Template.bind({});
-
-CollapsingDisabled.args = {
-  collapsible: false,
-  isCollapsed: true,
-} as IReqoreUIProviderProps & IQorusSidebarProps;
-
-export const OnRight = Template.bind({});
-OnRight.args = {
-  position: 'right',
-} as IReqoreUIProviderProps & IQorusSidebarProps;
-
-export const Bordered = Template.bind({});
-Bordered.args = {
-  bordered: true,
-} as IReqoreUIProviderProps & IQorusSidebarProps;
-
-export const Flat = Template.bind({});
-Flat.args = {
-  flat: true,
-} as IReqoreUIProviderProps & IQorusSidebarProps;
-
 export const Floating = Template.bind({});
 Floating.args = {
   floating: true,
   isOpen: true,
-  flat: true,
-  isCollapsed: false,
-  hasFloatingBackdrop: false,
-  closeOnItemClick: true,
-} as IReqoreUIProviderProps & IQorusSidebarProps;
+  hasFloatingBackground: true,
+};
