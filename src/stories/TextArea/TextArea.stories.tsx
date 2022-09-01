c89230bfbf2f513d5a8c89495b2bbd58579286d1
Foxhoundn <filip.witosz@gmail.com>
2022-09-01 07:38:06 +0000

    TextArea done

--- src/stories/TextArea/TextArea.stories.tsx
+++ src/stories/TextArea/TextArea.stories.tsx
@@ -1,34 +1,48 @@
-import { Meta, Story } from "@storybook/react/types-6-0";
-import React, { useState } from "react";
-import { IReqoreUIProviderProps } from "../../containers/UIProvider";
-import {
-  ReqoreContent,
-  ReqoreLayoutContent,
-  ReqoreTextarea,
-  ReqoreUIProvider,
-} from "../../index";
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { useState } from 'react';
+import { IReqoreTextareaProps } from '../../components/Textarea';
+import { ReqoreTextarea } from '../../index';
+import { argManager, DisabledArg, MinimalArg, SizeArg } from '../utils/args';
+
+const { createArg } = argManager<IReqoreTextareaProps>();
 
 export default {
-  title: "ReQore/TextArea",
-  args: {
-    theme: {
-      main: "#222222",
-    },
+  title: 'Components/TextArea',
+  argTypes: {
+    ...MinimalArg(),
+    ...DisabledArg,
+    ...SizeArg,
+    ...createArg('scaleWithContent', {
+      name: 'Scale with content',
+      description: 'Scale with content',
+      control: 'boolean',
+      defaultValue: true,
+    }),
+    ...createArg('fluid', {
+      name: 'Fluid',
+      description: 'Fluid',
+      control: 'boolean',
+      defaultValue: false,
+    }),
+    ...createArg('placeholder', {
+      name: 'Placeholder',
+      description: 'Placeholder',
+      control: 'text',
+      defaultValue: 'Placeholder',
+    }),
   },
-} as Meta;
+} as Meta<IReqoreTextareaProps>;
 
 const str =
-  "✔ Checking your system\n" +
-  "✔ Locating Application\n" +
-  "✔ Preparing native dependencies\n" +
-  "✔ Compiling Main Process Code\n" +
-  "✔ Launch Dev Servers\n" +
-  "✔ Compiling Preload Scripts\n" +
-  "✔ Launching Application\n";
+  '✔ Checking your system\n' +
+  '✔ Locating Application\n' +
+  '✔ Preparing native dependencies\n' +
+  '✔ Compiling Main Process Code\n' +
+  '✔ Launch Dev Servers\n' +
+  '✔ Compiling Preload Scripts\n' +
+  '✔ Launching Application\n';
 
-const Template: Story<IReqoreUIProviderProps> = (
-  args: IReqoreUIProviderProps
-) => {
+const Template: Story<IReqoreTextareaProps> = (args) => {
   const [value, setValue] = useState(str);
 
   const handleValueChange = (e) => {
@@ -36,73 +50,25 @@ const Template: Story<IReqoreUIProviderProps> = (
   };
 
   const handleValueClear = () => {
-    setValue("");
+    setValue('');
   };
 
   return (
-    <ReqoreUIProvider {...args}>
-      <ReqoreLayoutContent>
-        <ReqoreContent>
-          <div
-            style={{
-              padding: "20px",
-              width: "100%",
-              height: "100%",
-              overflow: "auto",
-            }}
-          >
-            <h4>Default</h4>
-            <ReqoreTextarea placeholder="Hello" size="small" />
-            <br />
-            <ReqoreTextarea placeholder="Hello" />
-            <br />
-            <ReqoreTextarea placeholder="Hello" size="big" />
-            <h4>Minimal</h4>
-            <ReqoreTextarea placeholder="Hello" minimal size="small" />
-            <br />
-            <ReqoreTextarea placeholder="Hello" minimal />
-            <br />
-            <ReqoreTextarea placeholder="Hello" minimal size="big" />
-            <h4>Disabled</h4>
-            <ReqoreTextarea placeholder="Hello" disabled />
-            <h4>Clearable</h4>
-            <ReqoreTextarea
-              scaleWithContent
-              onChange={handleValueChange}
-              onClearClick={handleValueClear}
-              value={value}
-            />
-            <h4>Scales with content</h4>
-            <ReqoreTextarea
-              placeholder="Hello"
-              scaleWithContent
-              value={str}
-              width={500}
-            />
-            <br />
-            <ReqoreTextarea minimal scaleWithContent value={str} width={500} />
-            <h4>Fluid</h4>
-            <ReqoreTextarea scaleWithContent value={str} fluid />
-            <br />
-          </div>
-        </ReqoreContent>
-      </ReqoreLayoutContent>
-    </ReqoreUIProvider>
+    <ReqoreTextarea
+      {...args}
+      onChange={handleValueChange}
+      onClearClick={handleValueClear}
+      value={value}
+    />
   );
 };
 
 export const Basic = Template.bind({});
-export const LightColor = Template.bind({});
-LightColor.args = {
-  theme: {
-    main: "#ffffff",
-  },
+export const Minimal = Template.bind({});
+Minimal.args = {
+  minimal: true,
 };
-
-export const CustomColor = Template.bind({});
-CustomColor.args = {
-  theme: {
-    main: "#0d0221",
-    color: "#2de2e6",
-  },
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
 };
