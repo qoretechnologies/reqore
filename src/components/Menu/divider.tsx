8cf43985c18fcdc856b6cec5f4f8c803bf956a07
Foxhoundn <filip.witosz@gmail.com>
2022-12-21 11:31:39 +0000

    Added sizes to dividers.

--- src/components/Menu/divider.tsx
+++ src/components/Menu/divider.tsx
@@ -1,28 +1,31 @@
 import React, { forwardRef } from 'react';
 import styled from 'styled-components';
+import { PADDING_FROM_SIZE } from '../../constants/sizes';
 import { getReadableColor } from '../../helpers/colors';
-import { IWithReqoreEffect } from '../../types/global';
+import { IWithReqoreEffect, IWithReqoreSize } from '../../types/global';
 import { IReqoreEffect, StyledTextEffect } from '../Effect';
 
 export interface IReqoreMenuDividerProps
   extends React.HTMLAttributes<HTMLDivElement>,
-    IWithReqoreEffect {
+    IWithReqoreEffect,
+    IWithReqoreSize {
   label?: string;
 }
 
 const StyledMenuDivider = styled(StyledTextEffect)`
   width: 100%;
-  padding: 8px 0;
+  padding: ${({ size }) => `${PADDING_FROM_SIZE[size]}px 0`};
   background-color: ${({ theme }) => theme.main};
 
   color: ${({ theme }) => getReadableColor(theme, undefined, undefined)};
 `;
 
 const ReqoreMenuDivider = forwardRef<HTMLDivElement, IReqoreMenuDividerProps>(
-  ({ label, className, effect, ...rest }: IReqoreMenuDividerProps, ref: any) => (
+  ({ label, className, effect, size = 'normal', ...rest }: IReqoreMenuDividerProps, ref: any) => (
     <StyledMenuDivider
       as='div'
       {...rest}
+      size={size}
       className={`${className || ''} reqore-menu-divider`}
       ref={ref}
       effect={
