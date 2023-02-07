58b3484b80ee300472df0f75bf0feae34875bac2
Foxhoundn <filip.witosz@gmail.com>
2023-02-07 09:41:30 +0000

    Put the mesasge back

--- src/stories/Message/Message.stories.tsx
+++ src/stories/Message/Message.stories.tsx
@@ -29,8 +29,12 @@ export default {
 const Template: Story<IReqoreMessageProps> = (args: IReqoreMessageProps) => {
   return (
     <>
-      <ReqoreMessage {...args} fixed onClick={noop}>
-        In to am attended desirous raptures
+      <ReqoreMessage {...args} onClick={noop}>
+        In to am attended desirous raptures declared diverted confined at. Collected instantly
+        remaining up certainly to necessary as. Over walk dull into son boy door went new. At or
+        happiness commanded daughters as. Is handsome an declared at received in extended vicinity
+        subjects. Into miss on he over been late pain an. Only week bore boy what fat case left use.
+        Match round scale now sex style far times. Your me past an much.
       </ReqoreMessage>
     </>
   );
