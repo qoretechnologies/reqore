f1a89e1fcf0b3dfd86eab49211eb6f1216cf668c
foxhoundn <filip.witosz@gmail.com>
2026-03-26 11:14:12 +0100

    refactor: remove unused imports and compact layout from ReqoreEmptyState

--- src/components/EmptyState/index.tsx
+++ src/components/EmptyState/index.tsx
@@ -1,18 +1,7 @@
-import { rgba } from 'polished';
 import React, { forwardRef, memo, useMemo } from 'react';
-import styled, { css } from 'styled-components';
 import { ReqorePanel } from '../..';
-import { PADDING_FROM_SIZE, RADIUS_FROM_SIZE, TSizes } from '../../constants/sizes';
-import { IReqoreTheme } from '../../constants/theme';
-import {
-  changeDarkness,
-  changeLightness,
-  getMainBackgroundColor,
-  getReadableColor,
-} from '../../helpers/colors';
 import { getOneHigherSize, getOneLessSize } from '../../helpers/utils';
 import { useReqoreTheme } from '../../hooks/useTheme';
-import { DisabledElement } from '../../styles';
 import {
   IReqoreDisabled,
   IReqoreIntent,
@@ -24,7 +13,7 @@ import {
 } from '../../types/global';
 import { IReqoreIconName } from '../../types/icons';
 import ReqoreControlGroup from '../ControlGroup';
-import { IReqoreEffect, StyledEffect, TReqoreEffectColor } from '../Effect';
+import { IReqoreEffect, TReqoreEffectColor } from '../Effect';
 import { ReqoreHeading } from '../Header';
 import ReqoreIcon, { IReqoreIconProps } from '../Icon';
 import { ReqoreP } from '../Paragraph';
@@ -58,51 +47,7 @@ export interface IReqoreEmptyStateProps
   transparent?: boolean;
   /** Background opacity */
   opacity?: number;
-  /** Whether to use a compact layout with less padding */
-  compact?: boolean;
 }
-
-interface IStyledEmptyStateWrapper {
-  theme: IReqoreTheme;
-  size: TSizes;
-  $fluid?: boolean;
-  disabled?: boolean;
-  $hasBackground?: boolean;
-  rounded?: boolean;
-  flat?: boolean;
-  intent?: string;
-  opacity?: number;
-  compact?: boolean;
-}
-
-const StyledEmptyStateWrapper = styled(StyledEffect)<IStyledEmptyStateWrapper>`
-  display: inline-flex;
-  justify-content: center;
-  width: ${({ $fluid }) => ($fluid ? '100%' : undefined)};
-
-  ${({ $hasBackground, theme, size, rounded, flat, intent, opacity = 1, compact }) =>
-    $hasBackground &&
-    css`
-      background-color: ${rgba(changeDarkness(getMainBackgroundColor(theme), 0.03), opacity)};
-      border-radius: ${rounded ? RADIUS_FROM_SIZE[size] : 0}px;
-      border: ${flat
-        ? undefined
-        : `1px solid ${changeLightness(
-            intent ? theme.intents[intent] : getMainBackgroundColor(theme),
-            0.08
-          )}`};
-      color: ${getReadableColor(theme, undefined, undefined, true)};
-      padding: ${compact
-        ? `${PADDING_FROM_SIZE[size] * 2}px ${PADDING_FROM_SIZE[size] * 3}px`
-        : `${PADDING_FROM_SIZE[size] * 4}px ${PADDING_FROM_SIZE[size] * 6}px`};
-    `}
-
-  ${({ disabled }) =>
-    disabled &&
-    css`
-      ${DisabledElement};
-    `}
-`;
 
 export const ReqoreEmptyState = memo(
   forwardRef<HTMLDivElement, IReqoreEmptyStateProps>(
@@ -125,7 +70,6 @@ export const ReqoreEmptyState = memo(
         transparent,
         opacity,
         effect,
-        compact,
         className,
         ...rest
       },
